291928cf9c04ef02b147c25bcbbf8df265b3b611
lucho.c <luchocordi@hotmail.com>
2021-09-04 13:44:37 -0300

    PetsApp - PetModel - Migrations

---
+++ Back/PETsAPI/PetsApp/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Pet(models.Model):
+    Id = models.AutoField(primary_key = True)
+    Name = models.CharField(max_length = 255)
+    Age = models.IntegerField()
+    
