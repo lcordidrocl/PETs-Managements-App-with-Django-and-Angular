9ef1911e4be4917f640807e4bd8743fe6aa40dff
lucho.c <luchocordi@hotmail.com>
2021-09-04 21:38:47 -0300

    basic routing and component definition

---
+++ UI/PetsAppUI/src/app/Shared/PETsAPI.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class PETsAPIService
+{
+    readonly baseURL = "http://127.0.0.1:8000/pet";
+
+    constructor(private httpClient: HttpClient) {}
+
+    getPets(): Observable<any[]>
+    {
+        return this.httpClient.get<any[]>(this.baseURL);
+    }
+
+    /* to be implemented   
+    getPet(id: number)
+    {
+        return this.httpClient.get<any[]>(this.baseURL + `/${id}` );
+    }
+    */
+
+    addPet(pet: any)
+    {
+        return this.httpClient.post(this.baseURL, pet);
+    }
+
+    deletePet(id: number)
+    {   
+        return this.httpClient.delete(this.baseURL + `/${id}` )
+    }
+}
--- UI/PetsAppUI/src/app/app.module.ts
+++ UI/PetsAppUI/src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { PETsAPIService } from './Shared/PETsAPI.service';
+import { PetsComponent } from './pets/pets.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PetsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [PETsAPIService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
