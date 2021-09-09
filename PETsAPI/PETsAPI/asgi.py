2f4debb3d0af99d5a6d12c7b33eab3f6f9b54c77
lucho.c <luchocordi@hotmail.com>
2021-09-09 00:02:35 -0300

    improved naming

--- PETsAPI/PETsAPI/asgi.py
+++ PETsAPI/PETsAPI/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PETsAPI.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PetsApi.settings')
 
 application = get_asgi_application()
